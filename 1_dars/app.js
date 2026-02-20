7886a92780fb4faed314bbe820cca5707db912b6
Ayubxon <qochqarovayubhon298@gamail.com>
2026-02-20 14:19:41 +0500

    yangi

---
+++ 1_dars/app.js
@@ -0,0 +1,124 @@
+// // a bekt
+// let obj = {
+//     name: "Bekzod",
+//     surname: "Bekzodov",
+//     age: 18,
+//     chiled:{
+//        name: "bekzod",
+//        n: "bekzod",
+//        hozi: "bekzod"
+//     }
+// }
+// console.log(obj.chiled.name);
+
+// 10 odiy objekt
+let obj1 = {
+  name: "Bekzod",
+  surname: "Bekzodov",
+  age: 18,
+  email: "bekzod@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj2 = {
+  name: "olmos",
+  surname: "gulomov",
+  age: 18,
+  email: "olmos@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj3 = {
+  name: "sardor",
+  surname: "sardorov",
+  age: 18,
+  email: "sardor@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj4 = {
+  name: "gulom",
+  surname: "gulomov",
+  age: 18,
+  email: "gulom@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj5 = {
+  name: "karim",
+  surname: "karimov",
+  age: 18,
+  email: "karim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj6 = {
+  name: "ibrohim",
+  surname: "ibrohimov",
+  age: 18,
+  email: "ibrohim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj7 = {
+  name: "ibrohim",
+  surname: "ibrohimov",
+  age: 18,
+  email: "ibrohim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj8 = {
+  name: "ibrohim",
+  surname: "ibrohimov",
+  age: 18,
+  email: "ibrohim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj9 = {
+  name: "ibrohim",
+  surname: "ibrohimov",
+  age: 18,
+  email: "ibrohim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+let obj10 = {
+  name: "ibrohim",
+  surname: "ibrohimov",
+  age: 18,
+  email: "ibrohim@gmail.com",
+  chaild: {
+    name: "ibrohim",
+    surname: "ibrohimov",
+  },
+};
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+console.log(obj4);
+console.log(obj5);
+console.log(obj6);
+console.log(obj7);
+console.log(obj8);
+console.log(obj9);
+console.log(obj10);
