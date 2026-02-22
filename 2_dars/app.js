79f37407ec93d3713795741e9cbb825be403d1ba
Ayubxon <qochqarovayubhon298@gamail.com>
2026-02-22 07:50:27 +0500

    yanada yangi

---
+++ 2_dars/app.js
@@ -0,0 +1,13 @@
+// let arr = [1, 2, 3, 4, "ali", true];
+// let arr2 = delete arr[2]; // kesadi va joy qoladi
+// arr[3] = 15;//elamet allish tirish
+
+// console.log(arr);
+
+// arr length
+// let arr1 = ["nok", "olma", "uzum", "anjir"]; //uzunligini aniqlash
+// console.log(arr1.length);
+
+// arry toString
+let arry1 = ["olma", "banan", "uzum", "anjir"];
+console.log(arry1);
