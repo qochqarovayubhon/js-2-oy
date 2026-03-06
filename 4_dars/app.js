f00c1371f6fa9b2d24dcf6b6895ae58a3d43cf73
Ayubxon <qochqarovayubhon298@gamail.com>
2026-03-06 14:04:54 +0500

    yana

---
+++ 4_dars/app.js
@@ -0,0 +1,18 @@
+///////////////// dom /////////////////
+// slektor
+let wrap = document.getElementsByClassName("div");
+console.log(wrap);
+
+let wrap2 = document.getElementsByTagName("div");
+console.log(wrap2);
+
+let wrap3 = document.getElementById("div");
+console.log(wrap3);
+
+let wrap4 = document.querySelector(".div");
+console.log(wrap4);
+
+
+//////////////////////////// text content/////////////////////////////
+// Inner text /////
+
